391a87edb2f1fc19744d5973ccc1c54f8d4008d4
achaochao <liangc0427@gmail.com>
2023-05-14 22:12:39 +0800

    update resnet

--- classification/backbone/__init__.py
+++ classification/backbone/__init__.py
@@ -2,10 +2,11 @@ from .lenet import LeNet
 from .alexnet import AlexNet
 from .vgg import VGG11, VGG13, VGG16, VGG19
 from .googlenet import GoogLeNet
-
+from .resnet import ResNet34, ResNet50, ResNet101, ResNet50_32x4d, ResNet101_32x8d
 
 __all__ = [
     "LeNet", "AlexNet",
     "VGG11", "VGG13", "VGG16", "VGG19",
-    "GoogLeNet"
+    "GoogLeNet",
+    "ResNet34", "ResNet50", "ResNet101", "ResNet50_32x4d", "ResNet101_32x8d"
 ]
